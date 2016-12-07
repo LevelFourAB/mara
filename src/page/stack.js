3d2ac2bfd94785b02f83f4559454c045a645ebe6
Andreas Holstenson <a@holstenson.se>
2016-12-07 13:33:04 +0100

    Smarter stack handling

--- src/page/stack.js
+++ src/page/stack.js
@@ -10,50 +10,97 @@ let disabled;
 
 let active = [];
 
+let highestZ = 0;
+
 function recreate() {
 	if(hidden) hidden.disengage();
 	if(disabled) disabled.disengage();
 
-	if(active.length === 0) return;
+	const top = active[active.length - 1];
 
-	hidden = ally.maintain.hidden({
-		filter: active
-	});
+	if(top) {
+		document.documentElement.classList.add('mara-no-scroll');
 
-	disabled = ally.maintain.disabled({
-		filter: active
-	});
+		const elements = active.map(item => item.element);
+		hidden = ally.maintain.hidden({
+			filter: top.element
+		});
+
+		disabled = ally.maintain.disabled({
+			filter: top.element
+		});
+
+		highestZ = 0;
+		active.forEach((item, idx) => {
+			highestZ = Math.max(highestZ, item.zIndex + 1);
+		});
+	} else {
+		document.documentElement.classList.remove('mara-no-scroll');
+	}
+}
+
+function hideActive() {
+	const item = active[active.length - 1];
+	if(! item) return;
+
+	if(item.options.onOverlayClick) {
+		item.options.onOverlayClick();
+	}
 }
 
 export default {
-	show: function(self, onOverlayClick) {
-		if(! overlay) {
-			overlay = document.createElement('div');
-			overlay.setAttribute('class', 'ml-page-overlay');
-			overlay.addEventListener('click', e => {
-				if(overlayClick) {
-					overlayClick();
-				}
-			});
-			document.body.appendChild(overlay);
+	show: function(self, options) {
+		if(typeof options === 'function') {
+			options = {
+				onOverlayClick: options
+			};
 		}
 
-		active.push(self);
-		recreate();
+		if(! options) {
+			options = {};
+		}
 
-		overlayClick = onOverlayClick;
-		overlay.classList.add('active');
-		document.documentElement.classList.add('ml-no-scroll');
+		const overlay = document.createElement('div');
+		overlay.className = 'mara-page-overlay ' + (options.className || '');
+		overlay.addEventListener('click', hideActive);
+
+		let closest = self.closest('[data-mara-stack]') || document.body;
+		closest.append(overlay);
+
+		if(highestZ === 0) {
+			// Figure out the Z that the main overlay has
+			const style = getComputedStyle(overlay);
+			const z = style.zIndex;
+			if(z !== 'auto') {
+				highestZ = Number(z);
+			}
+		}
+
+		const localZ = highestZ + 1;
+		overlay.style.zIndex = localZ;
+		self.style.zIndex = localZ + 1;
+		self.setAttribute('data-mara-stack', 'true');
+
+		active.push({
+			element: self,
+			options: options,
+			overlay: overlay,
+			zIndex: localZ
+		});
+		recreate();
 	},
 
 	hide: function(self) {
-		overlay.classList.remove('active');
-		document.documentElement.classList.remove('ml-no-scroll');
-
-		let idx = active.indexOf(self);
-		if(idx >= 0) {
-			active.splice(idx);
-			recreate();
+		for(let i=0; i<active.length; i++) {
+			const item = active[i];
+			if(item.element == self) {
+				active.splice(i, 1);
+				item.overlay.remove();
+				item.element.removeAttribute('data-mara-stack');
+				break;
+			}
 		}
+
+		recreate();
 	}
 };
