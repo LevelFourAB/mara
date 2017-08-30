f6d670c4b16938e1471e1102e42b882667a5e217
Andreas Holstenson <a@holstenson.se>
2017-08-30 10:10:41 +0200

    Using mixins with CE

--- ce.js
+++ ce.js
@@ -1,24 +1,63 @@
 'use strict';
 
-import chain from './util/chain';
+import { mix } from 'mixwith';
 
-function makeExtendable(parent) {
-	return class extends parent {
+/**
+ * Create a class that extends the given superclass that fulfills the
+ * constructor requirements of the Custom Elements polyfill and calls a
+ * init() method during creation.
+ *
+ */
+function create(superclass) {
+	const type = class extends superclass {
 		constructor(self) {
 			self = super(self);
 			self.init();
 			return self;
 		}
 
+		connectedCallback() {
+		}
+
+		disconnectedCallback() {
+		}
+
 		init() {
 		}
+	};
+
+	/*
+	 * Mix in some behaviour with this class.
+	 */
+	type.with = function(...args) {
+		return mix(type).with(...args);
+	};
+
+	return type;
+}
+
+/**
+ * Behaviour that adds domReadyCallback that is called when the contents of
+ * the element is ready.
+ *
+ * Uses setTimeout right now, but that might not be the best way to do this.
+ */
+const DOMReady = superclass => class extends superclass {
+	connectedCallback() {
+		super.connectedCallback();
+		setTimeout(() => this.domReadyCallback(), 0);
+	}
+
+	domReadyCallback() {
 	}
 };
 
 const define = window.customElements.define.bind(window.customElements);
-const HTMLCustomElement = makeExtendable(HTMLElement);
 export default {
-	HTMLCustomElement,
-	makeExtendable,
+	HTMLCustomElement: create(HTMLElement),
+	HTMLCustomSelectElement: create(HTMLSelectElement),
+	HTMLCustomInputElement: create(HTMLInputElement),
+	HTMLCustomTextAreaElement: create(HTMLTextAreaElement),
+	DOMReady,
 	define
 };
