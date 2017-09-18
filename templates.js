c67d6a03d247e7ae76184d91cb023dac0ff81d0c
Andreas Holstenson <a@holstenson.se>
2017-09-18 15:13:16 +0200

    Integrate with ShadyCSS if in use

--- templates.js
+++ templates.js
@@ -2,6 +2,9 @@ import { Mixin } from './ce';
 
 import { ShadowDOM } from './shadow-dom';
 
+/*
+ * Cache and resolution of templates based on the element name.
+ */
 const templates = new Map();
 function resolveTemplate(self) {
 	const name = self.localName;
@@ -11,9 +14,17 @@ function resolveTemplate(self) {
 	template = document.createElement('template');
 	template.innerHTML = self.constructor.templateHTML;
 	templates.set(name, template);
+
+	if(window.ShadyCSS) {
+		window.ShadyCSS.prepareTemplate(template, name);
+	}
 	return template;
 }
 
+/**
+ * Mixin that create a template by calling the static function `templateHTML`.
+ * The template is applied to the shadow root whenever an element is created.
+ */
 export const Template = Mixin(SuperClass => class extends SuperClass.with(ShadowDOM) {
 	static get templateHTML() {
 		return '';
@@ -25,5 +36,9 @@ export const Template = Mixin(SuperClass => class extends SuperClass.with(Shadow
 		const template = resolveTemplate(this);
 		const instance = template.content.cloneNode(true);
 		this.shadowRoot.appendChild(instance);
+
+		if(window.ShadyCSS) {
+			window.ShadyCSS.styleElement(this);
+		}
 	}
 });
